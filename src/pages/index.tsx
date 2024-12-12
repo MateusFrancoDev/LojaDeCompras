754f1cd83e615ea5f33133db54a230d44587b2c5
Mateus Franco <mateusfranco2@gmail.com>
2024-12-12 12:14:50 -0300

    Criação do Carrosel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 import { HomeContainer, Product } from "@/styles/pages/home";
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
+import 'keen-slider/keen-slider.min.css';
 
-import camisa1 from '../assets/camisa1.png';
-import camisa2 from '../assets/camisa1.png';
-import camisa3 from '../assets/camisa1.png';
-
+import camisa1 from '../assets/psg.webp';
+import camisa2 from '../assets/ceara.webp';
+import camisa3 from '../assets/corinthians.png';
 
 export default function Home() {
+  const [sliderRaf] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRaf} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camisa1} width={520} height={480} alt="" />
 
         <footer>
@@ -18,7 +26,25 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa XX</strong>
+          <span>R$ 80,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa XX</strong>
+          <span>R$ 80,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image src={camisa1} width={520} height={480} alt="" />
 
         <footer>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,7 +3,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) /2))',
   marginLeft: 'auto',
@@ -13,10 +12,10 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a',{
   background: 'linear-gradient(180deg, #4C97F3 0%, #000 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
+  minWidth: 540,
 
 // display: 'flex',
   alignItems: 'center',
